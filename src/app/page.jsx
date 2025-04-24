0796d9498eadf5874b8306bb01ef81971bb5efd1
Marcell Alessandro Reynoso Flores <135034636+MarcellReynoso@users.noreply.github.com>
2025-04-24 10:05:02 -0500

    Update page.jsx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,6 @@
 "use client";
-import IntroduccionSection from "./components/introduccionSection";
-import MenuPrincipalSection from "./components/menuPrincipalSection";
+import IntroduccionSection from "./components/IntroduccionSection";
+import MenuPrincipalSection from "./components/MenuPrincipalSection";
 import Navbar from "./components/Navbar";
 
 export default function Home() {
